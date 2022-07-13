7084052f8b6203009c57893890fa2fac9b0dbf3f
bryan-peguero <medranobryan22@gmail.com>
2022-07-12 20:27:59 -0400

    deploying this repo to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://bryan-peguero.github.io/rick_and_morty_project/',
   plugins: [react()]
 })
